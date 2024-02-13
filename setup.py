0e51aaccc1b6e67d65677de837560a42fd9bece6
= <=>
2024-02-13 23:47:14 +0000

    refactor for pypi + visual upgrade

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@
 import setuptools
 from setuptools import find_packages
 
-__ver__ = "1.0.1"
+__ver__ = "1.0.3"
+long_description=open('README.md').read()
 
 install_requires = [
         "humanize",
@@ -12,25 +13,39 @@ install_requires = [
         "pytesseract"]
 
 setuptools.setup(
-    name="mates",
+    name="pymates",
+    
+    author="Matt Harris",
+ 
+    
+    author_email="WorkMatthewJHarris94@gmail.com",
     version=__ver__,
     packages=find_packages(
         where='src',
     ),
     package_dir={"": "src"},
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+
     install_requires=install_requires,
     entry_points={
         'console_scripts': [
-            'mates = mates.main:run',
-            'testmates = mates.tests:tests',
+            'mates = pymates.main:run',
+            'testmates = pymates.tests:tests',
         ],
     },
     package_data={
-      "mates": ['*.png', '*.mp3', "./tests/*.png"],
+      "pymates": ['*.png', '*.mp3', "./tests/*.png"],
     },
     include_package_data=True,
-    include_dirs = ["/storage/emulated/0/Documents/MATES/src/mates/tests/"],
+    include_dirs = ["./src/pymates/tests/"],
     
+    license="MIT",
+    
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     )
